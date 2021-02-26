7e8d9111f69ee2dcbb6b36ef2e232e6157fd3139
Potato-Waffles <54952106+Potato-Waffles@users.noreply.github.com>
2021-02-26 13:58:52 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,227 @@
+input.onButtonPressed(Button.A, function () {
+    soundExpression.giggle.play()
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    Dice_Role = randint(1, 5)
+    Dice_rolled = true
+    basic.showNumber(Dice_Role)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(250)
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    control.reset()
+})
+let RingToneOn = false
+let Timer = 0
+let Tone = 0
+let Dice_rolled = false
+let Dice_Role = 0
+music.setBuiltInSpeakerEnabled(true)
+basic.pause(100)
+let Quiz_answered = true
+let Current_Question = 0
+Dice_Role = 0
+let Score = 0
+soundExpression.happy.play()
+basic.showString("Hi!")
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (Dice_rolled) {
+        if (Dice_Role == 1) {
+            Dice_rolled = false
+            basic.showString("Order Pizza!")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            basic.showString("DIY Time!")
+            Tone = 130
+            Timer = 720
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up!")
+        } else if (Dice_Role == 2) {
+            Dice_rolled = false
+            basic.showString("Ring tones!")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            RingToneOn = true
+        } else if (Dice_Role == 3) {
+            Dice_rolled = false
+            basic.showString("Paper planes")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            basic.showString("DIY")
+            Tone = 130
+            Timer = 360
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up")
+        } else if (Dice_Role == 4) {
+            Dice_rolled = false
+            basic.showString("Safe storage")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            Tone = 130
+            Timer = 720
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up")
+        } else if (Dice_Role == 5) {
+            Dice_rolled = false
+            basic.showString("Tower")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            Tone = 130
+            Timer = 360
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up")
+        } else if (Dice_Role == 6) {
+            Dice_rolled = false
+            basic.showString("Wine count")
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            Tone = 130
+            Timer = 360
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up")
+        } else if (Dice_Role == 7) {
+            Dice_rolled = false
+            basic.showString("Most shoes")
+            Quiz_answered = false
+            while (!(Quiz_answered)) {
+                if (input.buttonIsPressed(Button.B)) {
+                    Quiz_answered = true
+                }
+            }
+            Tone = 130
+            Timer = 360
+            for (let index = 0; index <= Timer / 5 - 2; index++) {
+                basic.showString("" + (Timer - index * 5))
+                basic.pause(5000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            for (let index = 0; index <= 10; index++) {
+                basic.showString("" + (10 - index))
+                basic.pause(1000)
+                Tone += 1
+                music.playTone(Tone, music.beat(BeatFraction.Whole))
+            }
+            basic.clearScreen()
+            basic.showString("Times Up")
+        }
+    }
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
