6c63e8fa93cb18f421b6df2f92e15f89f051700f
Potato-Waffles <54952106+Potato-Waffles@users.noreply.github.com>
2021-02-26 18:59:53 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
-    control.reset()
-})
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.AB, function () {
     soundExpression.giggle.play()
     basic.showIcon(IconNames.Heart)
     basic.pause(500)
@@ -12,20 +9,12 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     basic.pause(250)
 })
-input.onButtonPressed(Button.AB, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 let list: number[] = []
+let Current_Question = 0
 let Question_not_answered = false
-let MelodyAnswers: number[] = []
+let Score = 0
+let MelodyAnswers: boolean[] = []
 let Melodies2: string[][] = []
-let Melody_5: string[] = []
-let Melody_4: string[] = []
-let Melody_3: string[] = []
-let Melody_2: string[] = []
 let Melody_1: string[] = []
 let RingToneOn = false
 let Timer = 0
@@ -35,9 +24,7 @@ let Dice_Role = 0
 music.setBuiltInSpeakerEnabled(true)
 basic.pause(100)
 let Quiz_answered = true
-let Current_Question = 0
 Dice_Role = 0
-let Score = 0
 soundExpression.happy.play()
 basic.showString("Hi!")
 basic.showIcon(IconNames.Happy)
@@ -45,7 +32,7 @@ basic.forever(function () {
     if (Dice_rolled) {
         if (Dice_Role == 1) {
             Dice_rolled = false
-            basic.showString("Order Pizza!")
+            basic.showString("Pizza!")
             Quiz_answered = false
             while (!(Quiz_answered)) {
                 if (input.buttonIsPressed(Button.B)) {
@@ -61,8 +48,8 @@ basic.forever(function () {
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index2 = 0; index2 <= 10; index2++) {
+                basic.showString("" + (10 - index2))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -80,39 +67,11 @@ basic.forever(function () {
             }
             RingToneOn = true
             Melody_1 = ["C5 B A G F E D C ", "- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - "]
-            Melody_2 = ["- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - "]
-            Melody_3 = ["- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - "]
-            Melody_4 = ["- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - "]
-            Melody_5 = ["- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - ", "- - - - - - - - "]
-            Melodies2 = [Melody_1, Melody_2, Melody_3, Melody_4, Melody_5]
-            MelodyAnswers = [0, 1, 0, 0, 1]
+            Melodies2 = [Melody_1]
+            MelodyAnswers = [false]
             Score = 0
-            for (let index1 = 0; index1 <= 5; index1++) {
-                for (let index2 = 0; index2 <= 4; index2++) {
-                    music.playMelody(Melodies2[index1][index2], 120)
-                }
+            for (let index1 = 0; index1 <= 2; index1++) {
                 Question_not_answered = true
-                while (Question_not_answered) {
-                    if (input.buttonIsPressed(Button.A)) {
-                        Question_not_answered = false
-                        if (!(list[Current_Question])) {
-                            Score += 1
-                            basic.showIcon(IconNames.Yes)
-                        } else {
-                            basic.showIcon(IconNames.No)
-                        }
-                    } else if (input.buttonIsPressed(Button.B)) {
-                        Question_not_answered = false
-                        if (list[Current_Question]) {
-                            Score += 1
-                            basic.showIcon(IconNames.Yes)
-                        } else {
-                            basic.showIcon(IconNames.No)
-                        }
-                    }
-                    basic.showString("Score")
-                    basic.showNumber(Score)
-                }
             }
         } else if (Dice_Role == 3) {
             Dice_rolled = false
@@ -126,14 +85,14 @@ basic.forever(function () {
             basic.showString("DIY")
             Tone = 130
             Timer = 180
-            for (let index = 0; index <= Timer / 5 - 2; index++) {
-                basic.showString("" + (Timer - index * 5))
+            for (let index3 = 0; index3 <= Timer / 5 - 2; index3++) {
+                basic.showString("" + (Timer - index3 * 5))
                 basic.pause(5000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index4 = 0; index4 <= 10; index4++) {
+                basic.showString("" + (10 - index4))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -151,14 +110,14 @@ basic.forever(function () {
             }
             Tone = 130
             Timer = 600
-            for (let index = 0; index <= Timer / 5 - 2; index++) {
-                basic.showString("" + (Timer - index * 5))
+            for (let index5 = 0; index5 <= Timer / 5 - 2; index5++) {
+                basic.showString("" + (Timer - index5 * 5))
                 basic.pause(5000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index6 = 0; index6 <= 10; index6++) {
+                basic.showString("" + (10 - index6))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -176,14 +135,14 @@ basic.forever(function () {
             }
             Tone = 130
             Timer = 120
-            for (let index = 0; index <= Timer / 5 - 2; index++) {
-                basic.showString("" + (Timer - index * 5))
+            for (let index7 = 0; index7 <= Timer / 5 - 2; index7++) {
+                basic.showString("" + (Timer - index7 * 5))
                 basic.pause(5000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index8 = 0; index8 <= 10; index8++) {
+                basic.showString("" + (10 - index8))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -200,14 +159,14 @@ basic.forever(function () {
             }
             Tone = 130
             Timer = 180
-            for (let index = 0; index <= Timer / 5 - 2; index++) {
-                basic.showString("" + (Timer - index * 5))
+            for (let index9 = 0; index9 <= Timer / 5 - 2; index9++) {
+                basic.showString("" + (Timer - index9 * 5))
                 basic.pause(5000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index10 = 0; index10 <= 10; index10++) {
+                basic.showString("" + (10 - index10))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -225,14 +184,14 @@ basic.forever(function () {
             }
             Tone = 130
             Timer = 120
-            for (let index = 0; index <= Timer / 5 - 2; index++) {
-                basic.showString("" + (Timer - index * 5))
+            for (let index11 = 0; index11 <= Timer / 5 - 2; index11++) {
+                basic.showString("" + (Timer - index11 * 5))
                 basic.pause(5000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
             }
-            for (let index = 0; index <= 10; index++) {
-                basic.showString("" + (10 - index))
+            for (let index12 = 0; index12 <= 10; index12++) {
+                basic.showString("" + (10 - index12))
                 basic.pause(1000)
                 Tone += 1
                 music.playTone(Tone, music.beat(BeatFraction.Whole))
@@ -251,7 +210,7 @@ basic.forever(function () {
             }
             Current_Question = 1
             list = [1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0]
-            for (let index = 0; index <= list.length - 1; index++) {
+            for (let index13 = 0; index13 <= list.length - 1; index13++) {
                 Score = 0
                 Question_not_answered = true
                 basic.showNumber(Current_Question + 1)
